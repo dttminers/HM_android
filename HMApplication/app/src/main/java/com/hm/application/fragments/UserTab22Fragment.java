9436ee7fa908c9fed100f80e98096ea22b259270
Dineshdtt <dineshmaurya17@gmail.com>
2018-03-21 13:55:32 +0530

    user personal post data integration

--- HMApplication/app/src/main/java/com/hm/application/fragments/UserTab22Fragment.java
+++ HMApplication/app/src/main/java/com/hm/application/fragments/UserTab22Fragment.java
@@ -6,12 +6,30 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.hm.application.R;
+import com.hm.application.adapter.UserTab21Adapter;
+import com.hm.application.adapter.UserTab22Adapter;
+import com.hm.application.network.VolleySingleton;
 import com.hm.application.utils.CommonFunctions;
+
+import org.json.JSONArray;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserTab22Fragment extends Fragment {
 
@@ -36,7 +54,7 @@ public class UserTab22Fragment extends Fragment {
     private void checkInternetConnection() {
         try {
             if (CommonFunctions.isOnline(getContext())) {
-                new UserTab21Fragment.toUser21().execute();
+                new toUser22().execute();
             } else {
                 CommonFunctions.toDisplayToast(getResources().getString(R.string.lbl_no_check_internet), getContext());
             }
@@ -45,5 +63,77 @@ public class UserTab22Fragment extends Fragment {
         }
     }
 
-    private class toUser22 extends AsyncTask<>
+    private class toUser22 extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                VolleySingleton.getInstance(getContext())
+                        .addToRequestQueue(
+                                new StringRequest(Request.Method.POST,
+                                        "http://vnoi.in/hmapi/feed.php",
+                                        new Response.Listener<String>() {
+
+                                            @Override
+                                            public void onResponse(String response) {
+                                                try {
+                                                    Log.d("HmApp", "fetch_photos Res " + response);
+                                                    JSONArray array = new JSONArray(response.trim());
+                                                    if (array != null){
+                                                        if (array.length()> 0){
+                                                            RecyclerView mrvTab22 = getActivity().findViewById(R.id.rvUSerTab22);
+                                                            mrvTab22.setLayoutManager(new LinearLayoutManager(getContext()));
+                                                            mrvTab22.hasFixedSize();
+                                                            mrvTab22.setAdapter(new UserTab22Adapter(getContext(), array));
+                                                        } else {
+                                                            CommonFunctions.toDisplayToast("Ji", getContext());
+                                                        }
+                                                    }
+                                                    else {
+                                                        CommonFunctions.toDisplayToast("di", getContext());
+                                                    }
+                                                } catch (Exception| Error e){
+                                                    e.printStackTrace();
+                                                }
+                                            }
+                                        },
+                                        new Response.ErrorListener() {
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+                                                Log.d("HmApp", "Error " + error.getMessage());
+                                            }
+                                        }
+                                )
+                                {
+                                    @Override
+                                    protected Map<String, String> getParams() {
+                                        Map<String, String> params = new HashMap<String, String>();
+                                        params.put(getString(R.string.str_action_), getString(R.string.str_fetch_photos));
+                                        params.put(getString(R.string.str_uid), "20");
+                                        return params;
+                                    }
+
+                                    @Override
+                                    public Map<String, String> getHeaders() throws AuthFailureError {
+                                        Map<String, String> params = new HashMap<String, String>();
+                                        params.put(getString(R.string.str_header), getString(R.string.str_header_type));
+                                        // params.put("Content-Type","application/form-data");
+                                        return super.getHeaders();
+                                    }
+                                }
+                                , "fetch_photos");
+            } catch (Exception | Error e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
+
+
+
+
+
+
+
