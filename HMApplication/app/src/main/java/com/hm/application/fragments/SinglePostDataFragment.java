ee35922cbadb5239738b09fb40ed03128cb80d57
Dineshdtt <dineshmaurya17@gmail.com>
2018-03-23 11:26:32 +0530

    SinglePostfragment file
    done

--- HMApplication/app/src/main/java/com/hm/application/fragments/SinglePostDataFragment.java
+++ HMApplication/app/src/main/java/com/hm/application/fragments/SinglePostDataFragment.java
@@ -26,6 +26,7 @@ import com.hm.application.model.AppConstants;
 import com.hm.application.model.User;
 import com.hm.application.network.VolleySingleton;
 import com.hm.application.utils.CommonFunctions;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -67,7 +68,7 @@ public class SinglePostDataFragment extends Fragment {
             mcircle_img = (ImageView) getActivity().findViewById(R.id.circle_img);
             mtxt_label = (TextView) getActivity().findViewById(R.id.txt_label);
             mtxt_time_ago = (TextView) getActivity().findViewById(R.id.txt_time_ago);
-            mIvPost = getActivity().findViewById(R.id.image_single);
+            mIvPost = (ImageView) getActivity().findViewById(R.id.image_single);
 
             mllNumber_file = (LinearLayout) getActivity().findViewById(R.id.llNumber_file);
             mtxtNo_like = (TextView) getActivity().findViewById(R.id.txtNo_like);
@@ -88,7 +89,7 @@ public class SinglePostDataFragment extends Fragment {
         try {
             if (CommonFunctions.isOnline(getContext())) {
                 if (getArguments() != null) {
-                    new toDisplayPostData().execute();
+                    toDisplayPostData();//.execute();
                 } else {
                     CommonFunctions.toDisplayToast("Failed", getContext());
                     super.onDestroy();
@@ -101,35 +102,42 @@ public class SinglePostDataFragment extends Fragment {
         }
     }
 
-    private class toDisplayPostData extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                Log.d("HmAPp", " " + getArguments());
-                if (getArguments().getString(AppConstants.BUNDLE) != null) {
-                    JSONObject obj = new JSONObject(getArguments().getString(AppConstants.BUNDLE));
-                    Log.d("Hmapp", " obj " + obj);
+    private void toDisplayPostData() {
 
-                    if (!obj.isNull(getContext().getString(R.string.str_username_))) {
-                        mtxt_label.setText(obj.getString(getContext().getString(R.string.str_username_)));
-                    }
-                    if (!obj.isNull(getContext().getString(R.string.str_time))){
-                        mtxt_time_ago.setText(CommonFunctions.toSetDate(obj.getString(getContext().getString(R.string.str_time))));
-                    }
-                    if (!obj.isNull(getContext().getString(R.string.str_like_count))){
-                        mtxtNo_like.setText(obj.getString(getContext().getString(R.string.str_like_count))+ " " + getContext().getResources().getString(R.string.str_like));
-                    }
-                    if (!obj.isNull(getContext().getString(R.string.str_comment_count))){
-                        mtxtNo_comment.setText(obj.getString(getContext().getString(R.string.str_comment_count))+ " " + getContext().getString(R.string.str_comment));
-                    }
-                    if (!obj.isNull(getContext().getString(R.string.str_share_count))){
-                        mtxtNo_share.setText(obj.getString(getContext().getString(R.string.str_share_count)) + " " + getContext().getResources().getString(R.string.str_share));
-                    }
+        try {
+            Log.d("HmAPp", " " + getArguments());
+            if (getArguments().getString(AppConstants.BUNDLE) != null) {
+                JSONObject obj = new JSONObject(getArguments().getString(AppConstants.BUNDLE));
+                Log.d("Hmapp", " obj " + obj);
+
+                if (!obj.isNull(getContext().getString(R.string.str_username_))) {
+                    mtxt_label.setText(obj.getString(getContext().getString(R.string.str_username_)));
                 }
-            } catch (Exception | Error e) {
-                e.printStackTrace();
+                if (!obj.isNull(getContext().getString(R.string.str_time))) {
+                    mtxt_time_ago.setText(CommonFunctions.toSetDate(obj.getString(getContext().getString(R.string.str_time))));
+                }
+                if (!obj.isNull(getContext().getString(R.string.str_like_count))) {
+                    mtxtNo_like.setText(obj.getString(getContext().getString(R.string.str_like_count)) + " " + getContext().getResources().getString(R.string.str_like));
+                }
+                if (!obj.isNull(getContext().getString(R.string.str_comment_count))) {
+                    mtxtNo_comment.setText(obj.getString(getContext().getString(R.string.str_comment_count)) + " " + getContext().getString(R.string.str_comment));
+                }
+                if (!obj.isNull(getContext().getString(R.string.str_share_count))) {
+                    mtxtNo_share.setText(obj.getString(getContext().getString(R.string.str_share_count)) + " " + getContext().getResources().getString(R.string.str_share));
+                }
+                if (!obj.isNull(getContext().getString(R.string.str_image_url))) {
+                    Picasso.with(getContext())
+                            .load(AppConstants.URL + obj.getString(getContext().getString(R.string.str_image_url)).replaceAll("\\s", "%20"))
+                            .into(mcircle_img);
+                }
+                if (!obj.isNull(getContext().getString(R.string.str_image_url))) {
+                    Picasso.with(getContext())
+                            .load(AppConstants.URL + obj.getString(getContext().getString(R.string.str_image_url)).replaceAll("\\s", "%20"))
+                            .into(mIvPost);
+                }
             }
-            return null;
+        } catch (Exception | Error e) {
+            e.printStackTrace();
         }
     }
 }
